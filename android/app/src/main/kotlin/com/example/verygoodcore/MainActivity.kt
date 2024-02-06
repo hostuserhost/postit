991b29b49656738677cc79ede090ec313ea6cf7f
host <host@host.local>
2024-02-06 12:51:43 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.postit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:postit/app/app.dart';
+import 'package:postit/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
